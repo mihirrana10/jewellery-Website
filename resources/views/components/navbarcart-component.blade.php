fe36c105c666ef40dadf19d8315dc71136fbce9a
mihirj <mihirjariwala399@gmail.com>
2022-07-15 23:10:44 +0530

    firdt commit

---
+++ resources/views/components/navbarcart-component.blade.php
@@ -0,0 +1,185 @@
+<div class="navbar-cart">
+    <a class="icon-btn primary-icon-text icon-text-btn" href="javascript:void(0)">
+        <img src="{{url('user/assets/images/icon-svg/cart-1.svg')}}" alt="Icon">
+        <span class="icon-text text-style-1">88</span>
+    </a>
+
+    <div class="navbar-cart-dropdown">
+        <div class="checkout-style-2">
+            <div class="checkout-header d-flex justify-content-between">
+                <h6 class="title">Shopping Cart</h6>
+            </div>
+
+            <div class="checkout-table">
+                <table class="table">
+                    <tbody>
+                        <tr>
+                            <td class="checkout-product">
+                                <div class="product-cart d-flex">
+                                    <div class="product-thumb">
+                                        <img src="{{url('assets/images/product-cart/product-1.png')}}"
+                                            alt="Product">
+                                    </div>
+                                    <div class="product-content media-body">
+                                        <h5 class="title">
+                                            <a href="product-details-page.html">Hollow Port</a>
+                                        </h5>
+                                        <ul>
+                                            <li><span>Brown</span></li>
+                                            <li><span>XL</span></li>
+                                            <li>
+                                                <a class="delete" href="javascript:void(0)">
+                                                    <i class="mdi mdi-delete"></i>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </td>
+                            <td class="checkout-quantity">
+                                <div class="product-quantity d-inline-flex">
+                                    <button type="button" id="sub" class="sub">
+                                        <i class="mdi mdi-minus"></i>
+                                    </button>
+                                    <input type="text" value="1">
+                                    <button type="button" id="add" class="add">
+                                        <i class="mdi mdi-plus"></i>
+                                    </button>
+                                </div>
+                            </td>
+                            <td class="checkout-price">
+                                <p class="price">$36.00</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="checkout-product">
+                                <div class="product-cart d-flex">
+                                    <div class="product-thumb">
+                                        <img src="{{url('user/assets/images/product-cart/product-2.png')}}"
+                                            alt="Product">
+                                    </div>
+                                    <div class="product-content media-body">
+                                        <h5 class="title">
+                                            <a href="product-details-page.html">Mist Black Triblend</a>
+                                        </h5>
+                                        <ul>
+                                            <li><span>Brown</span></li>
+                                            <li><span>XL</span></li>
+                                            <li>
+                                                <a class="delete" href="javascript:void(0)">
+                                                    <i class="mdi mdi-delete"></i>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </td>
+                            <td class="checkout-quantity">
+                                <div class="product-quantity d-inline-flex">
+                                    <button type="button" id="sub" class="sub">
+                                        <i class="mdi mdi-minus"></i>
+                                    </button>
+                                    <input type="text" value="1">
+                                    <button type="button" id="add" class="add">
+                                        <i class="mdi mdi-plus"></i>
+                                    </button>
+                                </div>
+                            </td>
+                            <td class="checkout-price">
+                                <p class="price">$36.00</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="checkout-product">
+                                <div class="product-cart d-flex">
+                                    <div class="product-thumb">
+                                        <img src="{{url('user/assets/images/product-cart/product-3.png')}}"
+                                            alt="Product">
+                                    </div>
+                                    <div class="product-content media-body">
+                                        <h5 class="title">
+                                            <a href="product-details-page.html">Realm Bone</a>
+                                        </h5>
+                                        <ul>
+                                            <li><span>Brown</span></li>
+                                            <li><span>XL</span></li>
+                                            <li>
+                                                <a class="delete" href="javascript:void(0)">
+                                                    <i class="mdi mdi-delete"></i>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </td>
+                            <td class="checkout-quantity">
+                                <div class="product-quantity d-inline-flex">
+                                    <button type="button" id="sub" class="sub">
+                                        <i class="mdi mdi-minus"></i>
+                                    </button>
+                                    <input type="text" value="1">
+                                    <button type="button" id="add" class="add">
+                                        <i class="mdi mdi-plus"></i>
+                                    </button>
+                                </div>
+                            </td>
+                            <td class="checkout-price">
+                                <p class="price">$36.00</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="checkout-product">
+                                <div class="product-cart d-flex">
+                                    <div class="product-thumb">
+                                        <img src="{{url('user/assets/images/product-cart/product-4.png')}}"
+                                            alt="Product">
+                                    </div>
+                                    <div class="product-content media-body">
+                                        <h5 class="title">
+                                            <a href="product-details-page.html">Circular Sienna</a>
+                                        </h5>
+                                        <ul>
+                                            <li><span>Brown</span></li>
+                                            <li><span>XL</span></li>
+                                            <li>
+                                                <a class="delete" href="javascript:void(0)">
+                                                    <i class="mdi mdi-delete"></i>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </td>
+                            <td class="checkout-quantity">
+                                <div class="product-quantity d-inline-flex">
+                                    <button type="button" id="sub" class="sub">
+                                        <i class="mdi mdi-minus"></i>
+                                    </button>
+                                    <input type="text" value="1">
+                                    <button type="button" id="add" class="add">
+                                        <i class="mdi mdi-plus"></i>
+                                    </button>
+                                </div>
+                            </td>
+                            <td class="checkout-price">
+                                <p class="price">$36.00</p>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div class="checkout-footer">
+                <div class="checkout-sub-total d-flex justify-content-between">
+                    <p class="value">Subtotal Price:</p>
+                    <p class="price">$144</p>
+                </div>
+                <div class="checkout-btn">
+                    <a href="cart-page.html" class="main-btn primary-btn-border">View
+                        Cart</a>
+                    <a href="checkout-page.html" class="main-btn primary-btn">Checkout</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
